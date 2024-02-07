42ba10616353d3ea2d3954dc764afc1e8e510c8b
Anastasia Vandysheva <anastasiavandysheva@iMac-Anastasia.local>
2024-02-07 16:19:18 +0200

    adaptive

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,21 @@ $(document).ready(function() {
         appendArrows:".photo__slider-arrow"
     });
   });
+
+    // burger-menu
+$('.menu-icon').on('click', function(){
+  $('.menu-icon, .menu').toggleClass("active");
+  // $('body').toggleClass("overlay");
+})
+
+$(document).click(function(e) {
+  if (!$(e.target).hasClass("active") &&
+      $(e.target).parents(".site-nav").length === 0) {
+        $('.menu-icon, .menu').removeClass("active");
+        // $('body').removeClass("overlay");
+  }
+});
+$('.menu-item').on('click', function(){
+  $('.menu-icon, .menu').removeClass("active");
+  // $('body').removeClass("overlay");
+})  
